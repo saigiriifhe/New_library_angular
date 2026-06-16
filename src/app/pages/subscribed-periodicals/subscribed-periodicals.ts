739e6060381d44c3b3b986cc1d2e0f34b45eaa7f
saigiriifhe <saigiri@ifheindia.org>
2026-06-16 10:12:27 +0530

    subscribed Periodicals

--- src/app/pages/subscribed-periodicals/subscribed-periodicals.ts
+++ src/app/pages/subscribed-periodicals/subscribed-periodicals.ts
@@ -7,4 +7,38 @@ import { BackButton } from '../../shared/back-button/back-button';
   templateUrl: './subscribed-periodicals.html',
   styleUrl: './subscribed-periodicals.scss',
 })
-export class SubscribedPeriodicals {}
+export class SubscribedPeriodicals {
+  periodicals = [
+
+  {
+    title: 'Faculty of Management',
+    icon: 'bi bi-briefcase-fill',
+    file: 'assets/periodicals/N J yasaswy Memorial Library.pdf'
+  },
+
+  {
+    title: 'Faculty of Science & Technology',
+    icon: 'bi bi-cpu-fill',
+    file: 'assets/periodicals/FST Library.pdf'
+  },
+
+  {
+    title: 'Faculty of Law',
+    icon: 'bi bi-bank2',
+    file: 'assets/periodicals/FOL Periodicals list 2026.pdf'
+  },
+
+  {
+    title: 'ICFAI School of Architecture',
+    icon: 'bi bi-building',
+    file: 'assets/periodicals/ISArch Library.pdf'
+  },
+
+  {
+    title: 'IUP Journals',
+    icon: 'bi bi-journal-richtext',
+    file: 'assets/periodicals/IUP Periodicals.pdf'
+  }
+
+];
+}
